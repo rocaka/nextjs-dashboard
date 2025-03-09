70bff2c9f1f52b6d0a0e89aba2acd60c9cc97c35
timewalk <1170655076@qq.com>
2025-03-09 23:34:42 +0800

    修复字体问题

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,5 +2,6 @@ import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 import { Jersey_15 } from 'next/font/google';
  
-export const inter = Inter({ subsets: ['latin'] });
-export const jersey_15 = Jersey_15({ weight: ['400'], subsets: ['latin'] });
+export const inter = Inter({ subsets: ["latin"] });
+export const jersey_15 = Jersey_15({ weight: ["400"], subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] });
